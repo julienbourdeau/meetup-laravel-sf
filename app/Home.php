b795fc7f15ef3fb725b82ea1f76348111bc43f64
Julien Bourdeau <julien@sigerr.org>
2017-08-01 17:22:27 -0700

    :tada: make Home searchable

--- app/Home.php
+++ app/Home.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Home extends Model
 {
+    use Searchable;
+
     protected $casts = [
         'user' => 'array',
         'urls' => 'array',
