aac8918fefa83f54b512776882bf03d7c2cd472a
Julien Bourdeau <julien@sigerr.org>
2017-08-04 22:47:51 +0000

    Fix exporter

--- app/Console/Commands/AlgoliaExporter.php
+++ app/Console/Commands/AlgoliaExporter.php
@@ -56,11 +56,14 @@ class AlgoliaExporter extends Command
     {
         $this->index = $this->algolia->initIndex($this->argument('index'));
 
-        $data = $this->index->browse();
+        $data = [];
+        foreach ($this->index->browse('') as $hit) {
+            $data[] = $hit;
+        }
 
-        \File::put(resource_path('datasets/airbnb.json'), json_encode($data['hits'], JSON_PRETTY_PRINT));
+        \File::put(resource_path('datasets/airbnb.json'), json_encode($data, JSON_PRETTY_PRINT));
 
-        $keys = collect($data['hits'][0])->keys()->all();
+        $keys = collect($data[0])->keys()->all();
         \File::put(resource_path('datasets/airbnb_keys.json'), json_encode($keys, JSON_PRETTY_PRINT));
     }
 }
